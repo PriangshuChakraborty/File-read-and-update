c6f71d1f7bc92a9d53631b090746cd0b7ca56041
PriangshuChakraborty <cpriangshu@gmail.com>
2024-05-22 22:50:39 +0530

    diff file add

--- index.js
+++ index.js
@@ -2,16 +2,23 @@ const express = require('express');
 const app = express();
 const cors = require('cors')
 const fs = require('fs')
-const path = require('path')
 
 app.use(cors())
 app.use(express.json())
 
 const port = 3000;
 
-const filePath = path.join(__dirname, 'hello.txt');
+const secret = []
 
 app.get('/', async (req, res) => {
+    const filePath = req.body.fileName;
+    const password = req.body.password;
+
+    const findFile = secret.find((file) => file.fileName === filePath && file.password === password)
+    if (!findFile) {
+        res.status(404).send('File not found or password is incorrect');
+        return;
+    }
     await fs.readFile(filePath, 'utf8', (err, data) => {
         if (err) {
             res.status(500).send('Error reading file');
@@ -21,10 +28,36 @@ app.get('/', async (req, res) => {
     });
 });
 
+app.post('/', async (req, res) => {
+    const fileName = req.body.fileName;
+    const text = req.body.text;
+    const password = req.body.password;
+
+    const includesFileName = secret.some(obj => obj.fileName === fileName);
+    const includesPassword = secret.some(obj => obj.password === password);
+
+    if (includesFileName || includesPassword) {
+        res.status(400).send('File already exists or password is already used');
+        return;
+    }
+    await fs.writeFile(fileName, text, (err) => {
+        if (err) throw err;
+        secret.push({ fileName: fileName, password: password })
+        res.send(`File ${fileName} created and its text is ${text}`);
+    });
+});
+
 app.put('/', async (req, res) => {
     const text = req.body.text;
+    const filePath = req.body.fileName;
+    const password = req.body.password;
     if (!text) {
         res.status(400).send('Text is required');
+        return;
+    }
+    const findFile = secret.find((file) => file.fileName === filePath && file.password === password)
+    if (!findFile) {
+        res.status(404).send('File not found or password is incorrect');
         return;
     }
     await fs.writeFile(filePath, text, (err) => {
@@ -32,7 +65,7 @@ app.put('/', async (req, res) => {
             res.status(500).send('Error writing file');
             return;
         }
-        res.send('File written');
+        res.send(`File written`);
     });
 });
 
